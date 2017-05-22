cb3d660e804d55142931777af81a76371cb11e5d
harrytang <harry@modernkernel.com>
2017-05-22 17:34:35 +0700

    - improve cronjob

--- cronjobs/TaskCloseTicket.php
+++ cronjobs/TaskCloseTicket.php
@@ -8,17 +8,16 @@ use modernkernel\support\models\Ticket;
 
 
 $local = Core::isLocalhost();
-$time = $local ? '* * * * *' : '0 8 * * *';
+$time = $local ? '* * * * *' : '0 20 * * *';
 
 
 $schedule->call(function (\yii\console\Application $app) {
-    $output = '';
-    $day=7;
-    $time=time()-$day*86400;
+    $period=7*86400; // 7 days
+    $point=time()-$period;
 
     $tickets = Ticket::find()
         ->where(['status'=>Ticket::STATUS_WAITING])
-        ->andWhere('updated_at<:time', [':time'=>$time])
+        ->andWhere('updated_at<:point', [':point'=>$point])
         ->all();
 
     if ($tickets) {
@@ -37,5 +36,15 @@ $schedule->call(function (\yii\console\Application $app) {
     if (empty($output)) {
         $output = $app->getModule('support')->t('Nothing to do');
     }
-    echo $app->getModule('support')->t(basename(__FILE__, '.php') . ': ' . $output . "\n\n");
+    $log = new \common\models\TaskLog();
+    $log->task = basename(__FILE__, '.php');
+    $log->result = $output;
+    $log->save();
+    /* delete old logs never bad */
+    $period = 30 * 24 * 60 * 60; // 30 days
+    $point = time() - $period;
+    \common\models\TaskLog::deleteAll('task=:task AND created_at<=:point', [
+        ':task' => basename(__FILE__, '.php'),
+        ':point' => $point
+    ]);
 })->cron($time);
